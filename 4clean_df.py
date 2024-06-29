dfa812d075fb21a61b1bb0c2bcc76f3adf3d12d1
Ubuntu <ubuntu@ip-172-31-14-189.eu-west-1.compute.internal>
2024-06-29 15:33:32 +0000

    in progress

--- 4clean_df.py
+++ 4clean_df.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Charger le fichier CSV depuis le chemin local
-file_path = '3alibaba.csv'
+file_path = '3rakuten.csv'
 df = pd.read_csv(file_path)
 
 # Convertir la colonne de date en format datetime
@@ -14,7 +14,7 @@ df['date'] = df['date'].dt.strftime('%d-%m-%Y')
 df = df.drop(columns=['text'])
 
 # Enregistrer le dataframe nettoyé dans un nouveau fichier CSV
-cleaned_file_path = '4cleaned_data.csv'
+cleaned_file_path = '4rakuten.csv'
 df.to_csv(cleaned_file_path, index=False)
 
 print("Le dataframe a été nettoyé et sauvegardé avec succès.")
